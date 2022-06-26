d28c2ab2f45ec3140c076d5b3b614e434ed6dc8d
Fernando Daciuk <f.daciuk@gmail.com>
2022-06-26 14:46:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,117 @@
+<h1 align="center">
+  🦀 sls-rust
+</h1>
+
+<p align="center">
+   A ⚡ <a href="https://www.serverless.com/framework/docs/">Serverless framework</a> ⚡ plugin for <a href="https://www.rust-lang.org/">Rust</a> applications
+</p>
+
+> Note: this plugin was inspired on [softprops/serverless-rust](https://github.com/softprops/serverless-rust). 
+> Since the `serverless-rust` plugin is not activelly mantained, I created this 
+> one to work with minimal effort as possible: without docker, and probably only 
+> run on Linux (not tested on other OS). Great for CI environments.
+
+## 📦 Install
+
+You should put the `serverless.yml` file outside the Rust project directory:
+
+```console
+.
+├── your_rust_project
+│   ├── src
+│   │   └── main.rs
+│   └── Cargo.toml
+├── package.json
+└── serverless.yml
+```
+
+Install the plugin inside your serverless project with npm.
+
+```console
+$ npm i -D sls-rust
+```
+
+💡The `-D` flag adds it to your development dependencies in npm speak
+
+💡 This plugin assumes you are building Rustlang lambdas targeting the AWS Lambda "provided.al2" runtime. The [AWS Lambda Rust Runtime](https://github.com/awslabs/aws-lambda-rust-runtime) makes this easy.
+
+Add the following to your serverless project's `serverless.yml` file
+
+```yaml
+service: demo
+frameworkVersion: '3'
+configValidationMode: error
+
+provider:
+  name: aws
+  memorySize: 128
+  region: us-east-1
+
+plugins:
+  # this registers the plugin with serverless
+  - sls-rust
+
+# creates one artifact for each function
+package:
+  individually: true
+
+functions:
+  test:
+    # handler value syntax is `{rust_project_path}.{cargo_package_name}`
+    handler: rust_project_path.cargo_package_name
+    # you must use tags.rust = true to make this plugin works:
+    tags:
+      rust: true
+```
+
+> 💡 The Rust Lambda runtime requires a binary named `bootstrap`. This plugin renames the binary cargo builds to `bootstrap` for you. You do **not** need to do this manually in your `Cargo.toml` configuration file.
+
+In order to use this mode its expected that you install the `x86_64-unknown-linux-musl` target on all platforms locally with
+
+```sh
+$ rustup target add x86_64-unknown-linux-musl
+```
+
+On linux platforms, you will need to install musl-tools
+
+```sh
+$ sudo apt-get update && sudo apt-get install -y musl-tools
+```
+
+On Mac OSX, you will need to install a MUSL cross compilation toolchain
+
+```sh
+$ brew install filosottile/musl-cross/musl-cross
+```
+
+## 🤸 usage
+
+Every [serverless workflow command](https://serverless.com/framework/docs/providers/aws/guide/workflow/) should work out of the box.
+
+### invoke your lambdas locally
+
+```sh
+$ npx serverless invoke local -f hello -d '{"hello":"world"}'
+```
+
+### deploy your lambdas to the cloud
+
+```sh
+$ npx serverless deploy
+```
+
+### invoke your lambdas in the cloud directly
+
+```sh
+$ npx serverless invoke -f hello -d '{"hello":"world"}'
+```
+
+### view your lambdas logs
+
+```sh
+$ npx serverless logs -f hello
+```
+
+## License 
+
+MIT
---
+++ sls-rust.js
@@ -0,0 +1,146 @@
+'use strict'
+
+const { spawn } = require('node:child_process')
+const { join } = require('node:path')
+
+class SlsRust {
+  constructor (serverless, options) {
+    this.serverless = serverless
+    this.log = serverless.cli.log
+    this.options = options
+    this.targetRuntime = 'x86_64-unknown-linux-musl'
+    this.hooks = {
+      'before:package:createDeploymentArtifacts': this.buildPrepare.bind(this),
+      'before:deploy:function:packageFunction': this.buildPrepare.bind(this),
+      'before:offline:start': this.buildPrepare.bind(this),
+      'before:offline:start:init': this.buildPrepare.bind(this),
+    }
+
+
+    this.serverless.service.package.excludeDevDependencies = false
+  }
+
+  async buildPrepare () {
+    const service = this.serverless.service
+    if (service.provider.name !== 'aws') return
+    const rustFns = this.serverless.service.getAllFunctions().filter(fnName => {
+      const fn = service.getFunction(fnName)
+      return fn.tags?.rust === 'true'
+    })
+
+    if (rustFns.length === 0) {
+      throw new SlsRustPluginNoRustFnsError()
+    }
+
+    const buildPromises = rustFns.map(fnName => {
+      const fn = this.serverless.service.getFunction(fnName)
+      return this.build(fn)
+    })
+
+    await Promise.all(buildPromises)
+    this.log('finished building all rust functions!')
+  }
+
+  runCommand ({ command, cwd }) {
+    const [mainCommand, ...args] = command.split(' ')
+    return new Promise((resolve, reject) => {
+      const build = spawn(mainCommand, args, { inherit: 'stdio', cwd })
+      build.on('error', (error) => {
+        reject(error.toString())
+      })
+      build.on('close', (code) => {
+        resolve(code)
+      })
+    })
+
+  }
+
+  async runBuildCommand ({ path, projectName }) {
+    try {
+      await this.runCommand({ 
+        command: `cargo build --release --target ${this.targetRuntime}`, 
+        cwd: path,
+      })
+    } catch (error) {
+      throw new Error(`Error building project ${projectName}: ${error}`)
+    }
+  }
+
+  async runZipArtifact ({ path, projectName }) {
+    try {
+      await this.runCommand({ command: `mv ${projectName} bootstrap`, cwd: path })
+      await this.runCommand({ command: `zip bootstrap.zip bootstrap`, cwd: path })
+    } catch (error) {
+      throw new Error(`Error trying to zip artefact in ${projectName}: ${error}`)
+    }
+  }
+
+  async build (fn) {
+    const { projectPath, projectName } = this.getProjectPathAndName(fn)
+    this.log(`Building Rust ${fn.handler} func...`)
+    const path = join('.', projectPath)
+    const targetPath = join(path, 'target', this.targetRuntime, 'release')
+    await this.runBuildCommand({ path, projectName })
+    await this.runZipArtifact({ path: targetPath, projectName })
+
+    const artifactPath = join(targetPath, 'bootstrap.zip')
+    fn.package = fn.package || {}
+    fn.package.artifact = artifactPath
+    this.log(`Finished building ${projectName}!`)
+  }
+
+  getProjectPathAndName (fn) {
+    const [projectPath, projectName] = fn.handler.split('.')
+    if (!projectPath || !projectName) {
+      throw new SlsRustPluginWrongHandlerError()
+    }
+
+    return { projectPath, projectName }
+  }
+}
+
+class SlsRustPluginMainError extends Error {
+  constructor ({ name, message }) {
+    super()
+    this.name = `SlsRustPlugin${name}Error`
+    this.message = `[sls-rust plugin] ${message}`
+  }
+}
+
+class SlsRustPluginNoRustFnsError extends SlsRustPluginMainError {
+  constructor () {
+    super({
+      name: 'NoRustFns',
+      message: [
+        'no Rust functions found. In order to use this plugin, you must put ',
+        '`tags.rust: true` in your function configuration, like this:',
+        `
+
+# serverless.yml
+functions:
+  rust:
+    handler: your_rust_project_name
+    runtime: provided.al2
+    tags:
+      rust: true
+      `
+      ].join('')
+    })
+  }
+}
+
+class SlsRustPluginWrongHandlerError extends SlsRustPluginMainError {
+  constructor () {
+    super({
+      name: 'WrongHandler',
+      message: [
+        'the handler of your function must follow the pattern: ',
+        'project_path.project_name, when `project_path` is the path of your ',
+        'project, and `project_name` is the name of your project in Cargo.toml.\n'
+      ].join('')
+    })
+  }
+}
+
+
+module.exports = SlsRust
